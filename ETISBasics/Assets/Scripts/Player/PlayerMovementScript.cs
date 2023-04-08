eed9d1883654a7878e97b01d5c02816239546e44
bluetoothless <sersaw@onet.pl>
2023-04-08 22:14:01 +0200

    Added player movement

---
+++ ETISBasics/Assets/Scripts/Player/PlayerMovementScript.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementScript : MonoBehaviour
+{
+    public CharacterController controller;
+
+    public float speed = 12f;
+    public float gravity;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    Vector3 velocity;
+    bool isGounded;
+
+    void Update()
+    {
+        isGounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 movementVector = transform.right * x + transform.forward * z;
+        
+        controller.Move(movementVector * speed * Time.deltaTime);
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
